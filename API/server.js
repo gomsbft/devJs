724846659310552fa8b1556374ce7270f0ffe033
한지원 <gomdeschool@naver.com>
2024-08-06 12:03:33 +0900

    API

--- API/server.js
+++ API/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const path = require("path"), async = require("async"), fs = require("fs");
 
-app.listen(3000, () => {
-  console.log("서버 실행!")
+app.listen(port, () => {
+  console.log(`서버 실행! http://localhost:${port}`)
 })
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(path.join(__dirname + "/index.html"))
 })
 
 app.get("/post", (req, res) => {
@@ -14,4 +16,8 @@ app.get("/post", (req, res) => {
 
 app.get("/shop", (req, res) => {
   res.send("쇼핑 페이지 입니다")
+})
+
+app.get("/self", (req, res) => {
+  res.send("자기 소개 페이지 입니다")
 })
