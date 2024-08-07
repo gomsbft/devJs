a2728416b63c0549585ac4f8e623b1a4276bbf3e
한지원 <gomdeschool@naver.com>
2024-08-07 16:24:07 +0900

    mon

--- mon/model/controller/userController.js
+++ mon/model/controller/userController.js
@@ -14,3 +14,31 @@ export const create = async(req, res) => {
     res.status(500).json({error: "연결 안됌"})
   }
 }
+
+export const read = async(req, res) => {
+  try {
+    const users = await User.find();
+    if(users.length === 0) {
+      return res.status(404).json({message: " Not Found"})
+    }
+    res.status(200).josn(users);
+  }catch(error) {
+    res.status(500).json({error: "서버 에러"});
+}
+}
+
+export const update = async(req, res) => {
+  try{
+    const id = req.params.id
+    const userExist = await User.findOne({_id: id});
+    if(!userExist) {
+      return res.status(404).json({message: "Not Found"})
+    }
+    const updateUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+    res.status(200).json(updateUser);
+  }catch(error) {
+    res.status(500).json({error: "서버 에러"});
+}
+}
