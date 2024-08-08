1b5386849536ad9eaeeb2193cd3a14403b2a1f26
한지원 <gomdeschool@naver.com>
2024-08-08 10:56:36 +0900

    ejs

--- ejs/server.js
+++ ejs/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(expressLayouts);
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   const data = {message: "helloworld"};
-  res.render("index", data);
+  res.render("index", { 
+    data,
+    people: [
+      {name: "Kim"},
+      {name: "Lee"},
+      {name: "Park"}
+    ]
+
+  });
 })
 
 app.listen(8000, () => {
