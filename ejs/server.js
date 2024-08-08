36f625bdf69a7555cdf5efe7a6705de02caa0faf
한지원 <gomdeschool@naver.com>
2024-08-08 12:18:53 +0900

    ejs

--- ejs/server.js
+++ ejs/server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const expressLayouts = require("express-ejs-layouts")
+const expressLayouts = require("express-ejs-layouts");
+// const cors = require("cors");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// app.use(cors());
+app.use(express.urlencoded({extended: true}));
 app.use(expressLayouts);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const data = {message: "helloworld"};
   res.render("index", { 
+    tasks: tasks,
     data,
     people: [
       {name: "Kim"},
@@ -20,6 +24,35 @@ app.get("/", (req, res) => {
     ]
 
   });
+})
+
+app.get("/abuot", (req, res) => {
+  res.render("about")
+})
+
+app.get("/form", (req, res) => {
+  res.render("form")
+})
+
+app.post("/submit", (req, res) => {
+  const {name, email} = req.body;
+  res.render("result", {name, email})
+})
+
+let tasks = [];
+
+app.post("/addTask", (req, res) => {
+  const newTask = req.body.newTask;
+  if(newTask) {
+    tasks.push(newTask);
+  }
+  res.redirect("/");
+})
+
+app.post("/deleteTask", (req, res) => {
+  const deleteTask = req.body.task;
+  tasks = tasks.filter(task => task !== deleteTask);
+  res.redirect("/");
 })
 
 app.listen(8000, () => {
